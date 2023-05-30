923ae9dc3632b762bb62922484860a82645aed45
wsh <1603535741@qq.com>
2023-05-30 17:58:24 +0800

    fixed: init position ui root

--- Runtime/ViewManager.cs
+++ Runtime/ViewManager.cs
@@ -33,6 +33,7 @@ namespace Wsh.View {
 
         public static void InitAsync(string uiRootPrefabPath, ViewConfigDefine viewConfigDefine, Action<string, GameObject, Action<GameObject>> instantiateAsyncFunc, Func<GameObject, GameObject, GameObject> instantiateFunc, Action<ViewManager> onComplete) {
             instantiateAsyncFunc(uiRootPrefabPath, null, root => {
+                root.transform.position = new Vector3(5000, 0, 0);
                 ViewManager viewManger = root.GetComponent<ViewManager>();
                 viewManger.Init(viewConfigDefine, instantiateAsyncFunc, instantiateFunc);
                 onComplete?.Invoke(viewManger);
